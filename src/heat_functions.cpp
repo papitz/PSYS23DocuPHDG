d8c09788e1786c7974884727b0e272dc988e0020
Dennis Gottfried <dennis.gottfried@student.htw-berlin.de>
2023-09-07 14:06:56 -0700

    Added documentation for heat functions

--- src/heat_functions.cpp
+++ src/heat_functions.cpp
@@ -14,6 +14,17 @@ namespace heatFunctions {
 using namespace cv;
 using namespace std;
 
+/**
+ * @brief calculate the temperature of the tile based on its neighbors
+ *
+ * @param[in,out] tile value of the tile to calculate
+ * @param[in] up neighbor
+ * @param[in] left neighbor
+ * @param[in] right neighbor
+ * @param[in] down neighbor
+ * @param[in] heatTransferConstant constant that decides how much heat is transfered
+ * @return new temp of the tile
+ */
 float calculateNextTempOfTile(float tile, float up, float left, float right,
                               float down, float heatTransferConstant) {
 
@@ -23,10 +34,30 @@ float calculateNextTempOfTile(float tile, float up, float left, float right,
     return newTile;
 }
 
+/**
+ * @brief Check if a value is on the rim of the matrix
+ *
+ * @param[in] value location of the value
+ * @param[in] dimensions dimension of the array
+ * @return boolean that shows if the value is either zero or smaller or on the far side of the array
+ */
 bool zeroOrRim(int value, int dimensions) {
     return value <= 0 || value >= dimensions - 1;
 }
 
+/**
+ * @deprecated because this does not use the HeatMatrix class
+ * @brief calculate the next step of the heatMatrix
+ *
+ * @param[in,out] heatMatrix heatMatrix that goes in
+ * @param[in,out] updatedHeatMatrix second heatMatrix so we can check for Convergence
+ * @param[in] rows amount of rows
+ * @param[in] cols amount of cols
+ * @param[in] heatTransferConstant constant that decides how much heat is transfered
+ * @param[in] parallelFlag flag that decides if things are calculated in parallel or not
+ * @param[in] convergenceLimit limit at which delta of temp we count the matrix as converged
+ * @return if matrix is converged or not
+ */
 bool calculateHeatMatrix(float **heatMatrix, float **updatedHeatMatrix,
                          int rows, int cols, float heatTransferConstant,
                          bool parallelFlag, float convergenceLimit) {
@@ -80,6 +111,18 @@ bool calculateHeatMatrix(float **heatMatrix, float **updatedHeatMatrix,
     return false;
 }
 
+/**
+ * @brief calculate the next step of the heatMatrix
+ *
+ * @param[in,out] heatMatrix heatMatrix that goes in
+ * @param[in,out] updatedHeatMatrix second heatMatrix so we can check for Convergence
+ * @param[in] rows amount of rows
+ * @param[in] cols amount of cols
+ * @param[in] heatTransferConstant constant that decides how much heat is transfered
+ * @param[in] parallelFlag flag that decides if things are calculated in parallel or not
+ * @param[in] convergenceLimit limit at which delta of temp we count the matrix as converged
+ * @return if matrix is converged or not
+ */
 bool calculateHeatMatrix(HeatMatrix &heatMatrix, HeatMatrix &tmpHeatMatrix,
                          int rows, int cols, float heatTransferConstant,
                          bool parallelFlag, float convergenceLimit) {
@@ -107,38 +150,17 @@ bool calculateHeatMatrix(HeatMatrix &heatMatrix, HeatMatrix &tmpHeatMatrix,
     }
 
     heatMatrix.swap(tmpHeatMatrix);
-    /* TODO: Convergence check with reduction*/
-
-    /* HeatMatrix tmpPointer = heatMatrix; */
-    /* heatMatrix = updatedHeatMatrix; */
-    /* updatedHeatMatrix = tmpPointer; */
-    /* bool converged = true; */
-    /* auto stop = high_resolution_clock::now(); */
-    /* auto duration = duration_cast<milliseconds>(stop - start); */
-    /* printf("Took %li ms\n", duration.count()); */
-
-    // Update the heat matrix
-    /* for (int i = 0; i < rows; i++) { */
-    /*     /* heatMatrix[i] = new float[cols]; */
-    /*     for (int j = 0; j < cols; j++) { */
-    /*         if (converged && (heatMatrix[i][j] - updatedHeatMatrix[i][j] < */
-    /*                               -convergenceLimit || */
-    /*                           heatMatrix[i][j] - updatedHeatMatrix[i][j] > */
-    /*                               convergenceLimit)) { */
-    /*             converged = false; */
-    /*         } */
-    /*         heatMatrix[i][j] = updatedHeatMatrix[i][j]; */
-    /*     } */
-    /* } */
 
     return heatMatrix.checkForConversion(tmpHeatMatrix, convergenceLimit, parallelFlag);
-    /* return false; */
 }
 
-/* bool checkForConvergence(float convergenceLimit, HeatMatrix oldMatrix, HeatMatrix newMatrix){  */
-/*      */
-/* } */
 
+/**
+ * @brief calculate a color depending on the temp of a tile
+ *
+ * @param[[TODO:direction]] temperature [TODO:description]
+ * @param[[TODO:direction]] pixel [TODO:description]
+ */
 void setColorForTemperature(float temperature, cv::Vec3b &pixel) {
     switch ((int)(temperature / 75)) {
     case 0:
