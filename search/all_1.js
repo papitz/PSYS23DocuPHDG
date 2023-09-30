e1168df0d2c1687219f5c841ef81fb08cf2f8171
papitz <65065967+papitz@users.noreply.github.com>
2023-09-30 07:08:10 +0000

    Deploying to gh-pages from @ papitz/PSYS23DocuPHDG@e856cc3f7c7fe0394a397106852297ea1c0b894d 🚀

--- search/all_1.js
+++ search/all_1.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['calculateheatmatrix_1',['calculateHeatMatrix',['../namespaceheatFunctions.html#add20c105c31e0134ad793efaf6bdd963',1,'heatFunctions::calculateHeatMatrix(float **heatMatrix, float **tmpHeatMatrix, int rows, int cols, float heatTransferConstant, bool parallelFlag, float convergenceLimit)'],['../namespaceheatFunctions.html#aff9ff1bcef855957cc10b9ac4a5b02d1',1,'heatFunctions::calculateHeatMatrix(HeatMatrix &amp;heatMatrix, HeatMatrix &amp;tmpHeatMatrix, int rows, int cols, float heatTransferConstant, bool parallelFlag, float convergenceLimit)']]],
-  ['calculateheatmatrixinnerfunction_2',['calculateHeatMatrixInnerFunction',['../namespaceheatFunctions.html#a69d594635afd5660a588c6dfc2c2d001',1,'heatFunctions']]],
+  ['calculateheatmatrixinnerfunction_2',['calculateHeatMatrixInnerFunction',['../namespaceheatFunctions.html#a503d22671abe20ce17057c54a0553ecd',1,'heatFunctions']]],
   ['calculatenexttempoftile_3',['calculateNextTempOfTile',['../namespaceheatFunctions.html#a5801ec52d8e5a2c301d6109201eaf6ae',1,'heatFunctions']]],
   ['checkforconversion_4',['checkForConversion',['../classHeatMatrix.html#aeeab2018fad42e5d00ec9a85c1805225',1,'HeatMatrix']]],
   ['collectmatricesaftermpicalc_5',['collectMatricesAfterMPICalc',['../classHeatMatrix.html#aabb0ea3f9d2f2793a8e6dc8d79986eed',1,'HeatMatrix']]],
